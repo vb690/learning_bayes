01fad342a444694bb44dcbb9997804b34769c074
vb690 <vb690@york.ac.uk>
2021-08-10 17:43:27 +0100

    First 6 chapters

---
+++ books/bayesian_cognitive_modelling/models/chapter_3.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+import pymc3 as pm
+
+
+def rate_estimation(obs_freq, n, **beta_kwargs):
+    """PyMC3 implementation of rate estimation.
+
+    Args:
+        - obs_freq: array of int, observed frequencies.
+        - n: int, number of trials that generated obs_freq.
+        - **beta_kwargs: keyword argument, parameters of the prior beta
+            distribution.
+
+    Returns:
+        - model: PyMC3 model for estimating rate p generating obs_freq
+    """
+    with pm.Model() as model:
+
+        p = pm.Beta(
+            'p',
+            **beta_kwargs
+        )
+        freq = pm.Binomial(
+            'frquency',
+            n=n,
+            p=p,
+            observed=obs_freq
+
+        )
+
+    return model
+
+
+def rate_two_groups(obs_freq_1, obs_freq_2, n_1, n_2, shared=False,
+                    **beta_kwargs):
+    """PyMC3 implementation of 2 groups rate estimation.
+
+    Args:
+        - obs_freq_1: array of int, observed frequencies for the first group.
+        - obs_freq_2: array of int, observed frequencies for the second group.
+        - n_1: int, number of trials that generated obs_freq_1.
+        - n_2: int, number of trials that generated obs_freq_2.
+        - shared: bool, controls if the estimated rate is shared or not.
+        - **beta_kwargs: keyword argument, parameters of the prior beta
+            distribution.
+
+    Returns:
+        - model: PyMC3 model for estimating rate p generating obs_freq_1 and
+            obs_freq_2.
+    """
+    with pm.Model() as model:
+
+        if shared:
+            p = pm.Beta(
+                'p',
+                **beta_kwargs
+            )
+            p_1 = p
+            p_2 = p
+        else:
+            p_1 = pm.Beta(
+                'p_1',
+                **beta_kwargs
+            )
+            p_2 = pm.Beta(
+                'p_2',
+                **beta_kwargs
+            )
+            delta_p = pm.Deterministic(
+                'delta_frequencies',
+                p_1 - p_2
+            )
+
+        freq_1 = pm.Binomial(
+            'frquency_1',
+            n=n_1,
+            p=p_1,
+            observed=obs_freq_1
+
+        )
+
+        freq_2 = pm.Binomial(
+            'frquency_2',
+            n=n_2,
+            p=p_2,
+            observed=obs_freq_2
+
+        )
+
+    return model
+
+
+def joint_rate_trails(obs_freq, max_trials, **beta_kwargs):
+    """PyMC3 implementation of joint estimation of rate and trials.
+
+    Args:
+        - obs_freq: array of int, observed frequencies.
+        - max_trials: int, maximum number of trials we want to explore. The
+            prior for max_trials will be un-informantive, for each possible
+            trial value its probability will be computed as 1/max_trials.
+        - **beta_kwargs: keyword argument, parameters of the prior beta
+            distribution.
+
+    Returns:
+        - model: PyMC3 model for jointly estimating p and trials
+            generating obs_freq.
+    """
+    p_cat = np.array([i/max_trials for i in range(1, max_trials+1)])
+    with pm.Model() as model:
+
+        p = pm.Beta(
+            'p',
+            **beta_kwargs
+        )
+        n = pm.Categorical(
+            'n',
+            p=p_cat
+        )
+        freq = pm.Binomial(
+            'frquency',
+            n=n,
+            p=p,
+            observed=obs_freq
+
+        )
+
+    return model
---
+++ books/bayesian_cognitive_modelling/models/chapter_5.py
@@ -0,0 +1,311 @@
+import numpy as np
+import numpy.ma as ma
+
+import pymc3 as pm
+
+
+def estimate_pearson_correlation(obs_measurements, lkj_chol_kwargs, mu_kwargs,
+                                 sigma_kwargs=None):
+    """PyMC3 implementation of pearson correlation estimation.
+
+    Args:
+        - obs_measurements: nD array, n observed measurements.
+        - lkj_chol_kwargs: dict, keyword arguments for the Cholesky
+            decomposition.
+        - mu_kwargs: dict, keyword arguments for a normal distrbution.
+        - sigma_kwargs: None or dict,  keyword arguments for a HalfCauchy
+            distrbution. If specified allow to model measurement error using
+            the value of the multivariate normal as input to as many normal
+            distrbution as there are measurements.
+
+    Returns
+        - model: PyMC3 model, computing the parameters of a multivariate
+            normal distribution from which obs_measurement_1 and
+            obs_measurement_2 are drawn. Cholesky decomposition
+            provide the correlation matrix. If sigmas are provided, measurement
+            error can be estimated.
+    """
+    with pm.Model() as model:
+
+        n = obs_measurements.shape[0]
+
+        chol, corr, stds = pm.LKJCholeskyCov(
+            'Cholesky decomposition',
+            n=n,
+            compute_corr=True,
+            **lkj_chol_kwargs
+        )
+        cov = pm.Deterministic(
+            'covariance',
+            chol.dot(chol.T)
+        )
+
+        mu = pm.Normal(
+            'mu',
+            shape=n,
+            **mu_kwargs
+        )
+
+        if sigma_kwargs is None:
+            measurements = pm.MvNormal(
+                'measurements',
+                mu=mu,
+                chol=chol,
+                observed=obs_measurements
+            )
+        else:
+            latent_measurements = pm.MvNormal(
+                'latent_measurements',
+                mu=mu,
+                chol=chol
+            )
+            sigma = pm.HalfCauchy(
+                'measurements_error',
+                shape=(n,),
+                **sigma_kwargs
+            )
+            measurements = pm.Normal(
+                'measurements',
+                mu=latent_measurements,
+                sigma=sigma,
+                shape=(n,)
+            )
+
+    return model
+
+
+def estimate_k_coef_agreement(obs_frequencies, **beta_kwargs):
+    """PyMC3 implementation of the kappa coefficient of agreement for a binary
+    decision process in which a gold standard is compared to a surrogate method
+    by means of n comparisons. The k coefficient is the ratio between the
+    observed agreement and the agreement it would have occoured by chance.
+    This could be paramtrized with a dirichlet prior
+    but we will use separate betas for clarity.
+
+    Args:
+        - obs_frequencies: 4D array, frequencies of possible agreement
+            outcomes: 00, 01, 11, 10.
+        - **beta_kwargs: keyword argument, parameters of the prior beta
+            distribution.
+
+    Returns:
+        - model: a PyMC3 model estimating  the underlying rate of each
+            agreement outcome as well as the derived kappa coefficient.
+    """
+    with pm.Model() as model:
+
+        n = np.sum(obs_frequencies)
+
+        # rate gold standard decides 1
+        alpha = pm.Beta(
+            'alpha',
+            **beta_kwargs
+        )
+        # rate gold standard decides 0
+        alpha_prime = pm.Deterministic(
+            '1-alpha',
+            1 - alpha
+        )
+        # rate when surrogate decides 1 and gold standard decides 1
+        beta = pm.Beta(
+            'beta',
+            **beta_kwargs
+        )
+        # rate when surrogate decides 0 and the gold standard decides 1
+        beta_prime = pm.Deterministic(
+            '1-beta',
+            1 - beta
+        )
+        # rate when surrogate decides 0 when the gold standard decides 0
+        gamma = pm.Beta(
+            'gamma',
+            **beta_kwargs
+        )
+        # rate when surrogate decides 1 and the gold standard decides 0
+        gamma_prime = pm.Deterministic(
+            '1-gamma',
+            1 - gamma
+        )
+
+        # agreement 11
+        p_a = pm.Deterministic(
+            '11',
+            alpha*beta
+        )
+        # agreement 00
+        p_b = pm.Deterministic(
+            '00',
+            alpha_prime*gamma
+        )
+        # disagreement 01
+        p_c = pm.Deterministic(
+            '10',
+            alpha*beta_prime
+        )
+        # disagreement 10
+        p_d = pm.Deterministic(
+            '10',
+            alpha_prime*gamma_prime
+        )
+
+        # computing k
+        total_agreement = pm.Deterministic(
+            'total_agreement',
+            (alpha*beta) + (alpha_prime*gamma)
+        )
+        chance_agreement = pm.Deterministic(
+            'chance_agreement',
+            (p_a + p_b) + (p_a + p_c) + (p_b + p_d) + (p_c + p_d)
+        )
+        k = pm.Deterministic(
+            'k',
+            (total_agreement - chance_agreement) / (1 - chance_agreement)
+        )
+
+        # generating observed frequencies from estimated rates of agreement
+        obs_frequencies = pm.Multinomial(
+            'obs_frequencies',
+            n=n,
+            p=[p_a, p_d, p_c, p_d],
+            observed=obs_frequencies
+        )
+
+    return model
+
+
+def estimate_change_point(observed_time_series, time_steps, mu_kwargs,
+                          sigma_kwargs):
+    """PyMC3 implementation of a single change point detection.
+
+    Args:
+        - observed_time_series: numpy array, values of the time series.
+        - time_steps: numpy array, time indices associated with the time
+            series.
+        - mu_kwargs: dict, keyword argument for a Normal distrbution.
+        - sigma_kwargs: dict, keyword arguments for an HalfCauchy distrbution.
+
+    Returns:
+        - model: a PyMC3 model, given a stationary time series the model
+            detects at which point in time there is a shift in the mu
+            parameter of the normal distribution generating the time series.
+    """
+    with pm.Model() as model:
+
+        time_steps = pm.Data(
+            'time_steps',
+            time_steps
+        )
+
+        mus = pm.Normal(
+            'mus',
+            shape=(2,),
+            **mu_kwargs
+        )
+        sigma = pm.HalfCauchy(
+            'sigma',
+            **sigma_kwargs
+        )
+
+        change_point = pm.Uniform(
+            'change_point',
+            pm.math.minimum(time_steps),
+            pm.math.maximum(time_steps)
+        )
+
+        mu_index = (time_steps > change_point) * 1
+
+        time_series = pm.Normal(
+            'observed_time_series',
+            mu=mus[mu_index],
+            sigma=sigma,
+            observed=observed_time_series
+        )
+
+    return model
+
+
+def estimate_censored_data(n_censored_attempts, observed_attempt, n,
+                           beta_kwargs, lower_bound, upper_bound):
+    """PyMC3 implementation of censored data generative model.
+
+    Args:
+        - observed_attempt: int, number of correct answers for the observed
+            attempt.
+        - n: int, number of possible correct answers.
+        - beta_kwargs: dict, keyword arguments for a beta distribution.
+        - lower_bound: int, lower bound for the unobserved data.
+        - upper_bound: int, upper_bound for the unobserved data.
+
+    Returns:
+        - model: a PyMC3 model, estimating the underlying probability of giving
+            a correct answer with the constrains of observing one succesfull
+            attempt over n_censored_attempts + 1.
+    """
+    with pm.Model() as model:
+
+        def censored_ll(n, n_censored_attempts, p, lower_bound, upper_bound):
+            """Log likelyhood for bounded binomial distribution.
+            """
+            binomial = pm.Binomial.dist(n=n, p=p)
+            bounds = np.arange(lower_bound, upper_bound + 1)
+            return binomial.logp(bounds).sum() * n_censored_attempts
+
+        p = pm.Beta(
+          'p',
+          **beta_kwargs
+        )
+
+        unobserved = pm.Potential(
+          'unobservd',
+          censored_ll(
+              n=n,
+              n_censored_attempts=n_censored_attempts,
+              p=p,
+              lower_bound=lower_bound,
+              upper_bound=upper_bound
+          )
+        )
+
+        observed_attempt = pm.Binomial(
+          'observed',
+          n=n,
+          p=p,
+          observed=observed_attempt
+        )
+
+    return model
+
+
+def estimate_recapture(observed_recaptured, first_sample, second_sample,
+                       categorical_kwargs):
+    """PyMC3 implementation of a capture-recapture model.
+
+    Args:
+        - observed_recaptured: int, number of recaptured elements from
+            first_sample.
+        - first_sample: int, number of sampled elements for the first time.
+        - second_sample: int, number of sampled elements for the second time.
+        - categorical_kwargs: dict, keyword arguments for a categorical
+            distribution.
+
+    Returns:
+        - model: a PyMC3 model, it estimates the size of a population from
+            which first_sample was drawn and subsequently second_sample was
+            drawn observing observed_recaptured elements.
+    """
+    with pm.Model() as model:
+
+        population_size = pm.Categorical(
+            'population_size',
+            **categorical_kwargs
+        )
+
+        recaptured = pm.HyperGeometric(
+            'recaptured',
+            n=second_sample,
+            x=first_sample,
+            t=population_size,
+            observed=observed_recaptured
+        )
+
+    return model
