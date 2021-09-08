344fc49c3f49e50c1f82c3b245b33e8c3afac45a
vb690 <vb690@york.ac.uk>
2021-09-08 18:23:37 +0100

    Last model chapter 9

--- books/bayesian_cognitive_modelling/modules/models/chapter_9.py
+++ books/bayesian_cognitive_modelling/modules/models/chapter_9.py
@@ -3,9 +3,11 @@ import numpy as np
 import pymc3 as pm
 
 
-def estimate_proportions_with(observed_answers, trials_n, normal_mu_kwargs,
-                              cauchy_sigma_kwargs,
-                              ):
+def estimate_proportions_with(observed_answers, trials_n,
+                              half_cauchy_sigma_rate_kwargs,
+                              half_normal_mu_rate_kwargs,
+                              half_cauchy_sigma_diff_kwargs,
+                              half_normal_mu_size_kwargs):
     """
     """
     grp_1_answers = observed_answers[0]
@@ -14,26 +16,24 @@ def estimate_proportions_with(observed_answers, trials_n, normal_mu_kwargs,
     grp_1_trials = trials_n[0]
     grp_2_trials = trials_n[1]
 
-    idx = np.array([i for i in range(len(grp_1_answers))])
-
     with pm.Model() as model:
 
         mu_rate = pm.HalfNormal(
             'mu_rate',
-            **normal_mu_kwargs
+            **half_normal_mu_rate_kwargs
         )
         sigma_rate = pm.HalfCauchy(
             'sigma_rate',
-            **cauchy_sigma_kwargs
+            **half_cauchy_sigma_rate_kwargs
         )
 
         size_diff = pm.HalfNormal(
             'size_diff',
-            **normal_mu_kwargs
+            **half_normal_mu_size_kwargs
         )
         sigma_diff = pm.HalfCauchy(
             'sigma_rate',
-            **cauchy_sigma_kwargs
+            **half_cauchy_sigma_diff_kwargs
         )
         mu_diff = pm.Deterministic(
             'mu_diff',
@@ -54,19 +54,91 @@ def estimate_proportions_with(observed_answers, trials_n, normal_mu_kwargs,
         )
         phi_grp_2 = pm.Deterministic(
             'phi_grp_2',
-            phi_grp_1[idx] + diff[idx]
+            phi_grp_1 + diff
         )
 
         theta_grp_1 = pm.Deterministic(
             'theta_grp_1',
             pm.sigmoid(
-                phi_grp_1[idx]
+                phi_grp_1
             )
         )
         theta_grp_2 = pm.Deterministic(
             'theta_grp_2',
             pm.sigmoid(
-                phi_grp_2[idx]
+                phi_grp_2
+            )
+        )
+
+        observed_grp_1 = pm.Binomial(
+            p=theta_grp_1,
+            n=grp_1_trials,
+            observed=grp_1_answers
+        )
+        observed_grp_2 = pm.Binomial(
+            p=theta_grp_2,
+            n=grp_2_trials,
+            observed=grp_2_answers
+        )
+
+    return model
+
+
+def estimate_proportions_betw(observed_answers, trials_n,
+                              half_cauchy_sigma_rate_kwargs,
+                              normal_mu_rate_kwargs,
+                              half_normal_mu_size_kwargs):
+    """
+    """
+    grp_1_answers = observed_answers[0]
+    grp_2_answers = observed_answers[1]
+
+    grp_1_trials = trials_n[0]
+    grp_2_trials = trials_n[1]
+
+    with pm.Model() as model:
+
+        mu_rate = pm.HalfNormal(
+            'mu_rate',
+            **normal_mu_rate_kwargs
+        )
+        sigma_rate = pm.HalfCauchy(
+            'sigma_rate',
+            **half_cauchy_sigma_rate_kwargs
+        )
+
+        size_diff = pm.HalfNormal(
+            'size_diff',
+            **half_normal_mu_size_kwargs
+        )
+        mu_diff = pm.Deterministic(
+            'mu_diff',
+            size_diff * sigma_rate
+        )
+
+        phi_grp_1 = pm.Normal(
+            'phi_grp_1',
+            mu=mu_rate + mu_diff,
+            sigma=sigma_rate,
+            shape=(len(grp_1_answers), )
+        )
+        phi_grp_2 = pm.Normal(
+            'phi_grp_2',
+            mu=mu_rate - mu_diff,
+            sigma=sigma_rate,
+            shape=(len(grp_2_answers), )
+        )
+
+        theta_grp_1 = pm.Deterministic(
+            'theta_grp_1',
+            pm.sigmoid(
+                phi_grp_1
+            )
+        )
+        theta_grp_2 = pm.Deterministic(
+            'theta_grp_2',
+            pm.sigmoid(
+                phi_grp_2
             )
         )
 
